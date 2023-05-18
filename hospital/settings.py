0bfa77d6c8a283fd161ee131ea785d597782e74b
mahmoudayman2001 <mahmod.ayman@icloud.com>
2023-05-18 15:02:40 +0300

    Initial commit

--- hospital/settings.py
+++ hospital/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = "Appoinment-create"
-LOGOUT_REDIRECT_URL = "signup"
+LOGOUT_REDIRECT_URL = "login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
